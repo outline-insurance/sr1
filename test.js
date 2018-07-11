312f0c86a97c227b0c76562e4e08cb44e8740d2f
Kevin Kwok <antimatter15@gmail.com>
2018-07-11 00:56:38 -0700

    adding jest & .babelrc

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-import { State, Store, Route } from './index'
+import { State as State_, Store as Store_, Route as Route_ } from './index'
 
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+test('throw for accessing globals', () => {
+  expect(() => {
+    State.get('hi')
+    Store.get('hi')
+    Route.get('hi')
+  }).toThrow()
 });
